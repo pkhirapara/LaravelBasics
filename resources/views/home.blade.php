b376afc3350608132f55a90a90a70d57731c86b1
pratik hirpara <pkhirparapk@outlook.com>
2021-03-04 14:13:23 +0100

    Vue Basics 101

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,8 +14,10 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <my-button text="My Button"><my-button>
+                   
                 </div>
+                
             </div>
         </div>
     </div>
